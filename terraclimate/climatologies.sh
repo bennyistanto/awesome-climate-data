1cec2206651a08901833bf6d9883f96ee986a245
Benny Istanto <bennyistanto@outlook.com>
2021-10-15 23:33:11 +0700

    Add files via upload

---
+++ terraclimate/climatologies.sh
@@ -0,0 +1,4 @@
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/climatologies/TerraClimate_mxm_climo.nc
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/climatologies/TerraClimate_mnm_climo.nc
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/climatologies/TerraClimate_def_climo.nc
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/climatologies/TerraClimate_aet_climo.nc
---
+++ terraclimate/layers.sh
@@ -0,0 +1,3 @@
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/layers/terraclim_dem.nc
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/layers/MODIS_landcover_2017.nc
+wget -c http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/layers/HFP2009_InTerraClimateGrids.nc
